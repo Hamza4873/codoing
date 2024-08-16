4d3bc40a14ce59c3aa81c14ed1cb2ec110093337
Hamza4873 <129904888+Hamza4873@users.noreply.github.com>
2024-08-15 21:29:51 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,47 +1,20 @@
-import pandas as pd
-import numpy as np
+from setuptools import setup, find_packages
 
-def create_flat_dataframe(data):
-    # Helper function to flatten the dictionary
-    def flatten_data(y):
-        out = {}
-
-        def flatten(x, name=''):
-            if isinstance(x, dict):
-                for a in x:
-                    flatten(x[a], name + a + '_')
-            elif isinstance(x, list):
-                for i, a in enumerate(x):
-                    flatten(a, name + str(i) + '_')
-            else:
-                out[name[:-1]] = x
-
-        flatten(y)
-        return out
-
-    # Flattening each item in the data list and creating a list of dictionaries
-    my_list = [flatten_data(d) for d in data['data']]
-
-    # Creating DataFrame from the list of dictionaries
-    df = pd.DataFrame(my_list)
-
-    # Ensuring all columns from the first item are present in the DataFrame
-    if my_list:
-        keylist = list(my_list[0].keys())
-        for key in keylist:
-            if key not in df.columns:
-                df[key] = np.nan
-
-    return df
-
-# Example usage
-data = {
-    'data': [
-        {'key1': 'value1', 'key2': {'subkey1': 'subvalue1'}},
-        {'key1': 'value2', 'key3': ['listitem1', 'listitem2']}
-    ]
-}
-
-# Call the function with the example data
-df = create_flat_dataframe(data)
-print(df)
+setup(
+    name='my_package',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.0.0',
+        'ipwhois>=1.0.0',
+        'shodan>=1.24.0',
+        'passivetotal>=1.0.0',
+        'pyyaml>=5.4',
+        'vt-py>=0.6.0',
+        'domaintools_api>=1.0.0',
+    ],
+    dependency_links=[
+        'https://<some_url>/simple/<package_name>'  # Custom index for a specific package
+    ],
+    python_requires='>=3.6',
+)
