3fa7814b4cfae0834c8f2622031668b15724fcf5
Hamza4873 <129904888+Hamza4873@users.noreply.github.com>
2024-08-23 11:45:45 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-# Sample multiselect widget
-dbutils.widgets.multiselect("selected_columns", "", ["col1", "col2", "col3_long_value", "col4_really_long_value"], "Select Columns")
-
-# Injecting custom CSS and JavaScript to widen the widget
-html_content = """
-<style>
-  .input-widget-multiselect { 
-    width: 500px !important;  /* Adjust the width as needed */
-  }
-</style>
-"""
-
-# Display the custom CSS to override the default widget width
-displayHTML(html_content)
+sample_value = flat_responses[0].get(field, "N/A")
+        print(f"{idx + 1}. {field} (Sample Value: {sample_value})")
