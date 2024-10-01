d2eccad9a4152a555d25f93e6255f6ed67953aad
Hamza4873 <129904888+Hamza4873@users.noreply.github.com>
2024-10-01 09:04:43 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import re
 
 text = "Some other text ‘Recipient’: [value, value,value]"
-match = re.search(r"Recipient\s*:\s*\[([^\]]+)\]", text)
+match = re.search(r"‘Recipient’\s*:\s*\[([^\]]+)\]", text)
 
 if match:
     print(match.group(1))  # Outputs: value, value,value
